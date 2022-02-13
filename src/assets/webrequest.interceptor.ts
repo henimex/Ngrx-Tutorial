f1f5c1b8907d0e93665227d82ab2fcf4bd68059e
Prasad Rao <pcr1393@gmail.com>
2022-02-13 18:37:22 +0530

    second commit

---
+++ src/assets/webrequest.interceptor.ts
@@ -0,0 +1,37 @@
+export class WebRequestInterceptor {
+  mockResponseArray: any = {
+    '/contacts': 'assets/contacts.json'
+  };
+
+
+  constructor() {
+    const func = (XHR: any): any => {
+      const that = this;
+      let senderUrl: string;
+      const open = XHR.prototype.open;
+      XHR.prototype.open = function(method: string, url: string, async: boolean, user: string, pass: string): void {
+        senderUrl = url;
+        const response = that.mockResponse(senderUrl);
+        if (response && that.islocalHost()) {
+          open.call(this, 'GET', response, true, user, pass);
+        } else {
+          open.apply(this, arguments);
+        }
+      }
+    };
+    func.apply(null, [XMLHttpRequest]);
+  }
+
+  mockResponse(url: string): string {
+    for (const key in this.mockResponseArray) {
+      if (url.indexOf(key) !== -1) {
+        return this.mockResponseArray[key];
+      }
+    }
+  }
+
+  islocalHost(): boolean {
+    return window.location.hostname === 'localhost';
+  }
+
+}
